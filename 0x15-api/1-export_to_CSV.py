8e0155b905f9066a7cc683d693ca9ae5584eeff3
amasin76 <amasin76@gmail.com>
2023-10-23 23:11:22 +0000

    Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+
+import requests
+import sys
+import csv
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print('Usage: ', sys.argv[0], '<employee id>')
+        sys.exit(1)
+
+    user_id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com'
+    user = requests.get(url + '/users/{}'.format(user_id)).json()
+    todos = requests.get(url + '/users/{}/todos'.format(user_id)).json()
+
+    with open('{}.csv'.format(user_id), 'w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            writer.writerow([user_id, user.get('name'),
+                            task.get('completed'), task.get('title')])
