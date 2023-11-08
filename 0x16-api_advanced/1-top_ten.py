92c8b872fd9a205367452fd10db12141767d9dbf
amasin76 <amasin76@gmail.com>
2023-11-08 01:52:38 +0000

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""prints the titles of the first 10 hot posts listed for a given subreddit"""
+import requests
+
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'custom'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    data = response.json()['data']['children']
+    for post in data:
+        print(post['data']['title'])
