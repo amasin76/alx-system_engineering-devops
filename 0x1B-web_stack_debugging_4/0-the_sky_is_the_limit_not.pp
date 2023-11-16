9d3ddabf1a55606ac3e696b9e94159dae80d95c1
amasin76 <amasin76@gmail.com>
2023-11-16 09:20:57 +0000

    Sky is the limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+# increase the upper limit
+exec { 'increase upper limit':
+  provider => shell,
+  command  => 'sed -i s/15/1000000/ /etc/default/nginx'
+}
+exec { 'restart nginx server':
+  provider => shell,
+  command  => 'service nginx restart'
+}
